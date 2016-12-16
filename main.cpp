9e0615c48310fa0ebbc79d8db1a7131931873214
Andy Conway <aconway01@users.noreply.github.com>
2016-12-16 14:22:07 -0500

    CSV output, defined methods for contrast metrics
    Created functions for michelson and rms contrast, added output to csv file, removed some comments, cleaned up code

--- main.cpp
+++ main.cpp
@@ -13,16 +13,15 @@ namespace fs = boost::filesystem;
 
 // returns random int on interval [a,b]
 int random(int a,int b) {
-	srand(time(NULL));
-	int c = b-a;
-	int num = rand() % c + a;
-	return num;
+        srand(time(NULL));
+        int c = b-a;
+        int num = rand() % c + a;
+        return num;
 }
 
 struct box {
-	int xmin, xmax, ymin, ymax;	
+        int xmin, xmax, ymin, ymax;    
 };
-
 
 // return the filenames of all files that have the specified extension
 // in the specified directory and all subdirectories
@@ -32,93 +31,159 @@ void get_all(const fs::path& root, const string& ext, vector<fs::path>& ret)
 
     fs::recursive_directory_iterator it(root);
     fs::recursive_directory_iterator endit;
-    
+
 
     while(it != endit)
     {
         if(fs::is_regular_file(*it) && it->path().extension() == ext)
-	{
-	fs::path path = canonical(it->path());
-	//ret.push_back(it->path().filename());
-	ret.push_back(path);
-	}
+        {
+        fs::path path = canonical(it->path());
+        //ret.push_back(it->path().filename());
+        ret.push_back(path);
+        }
         ++it;
 
     }
 
 }
 
+// returns contrast from set of points using michelson metric (Gene Ware)
+double michelson_contrast(cv::Mat image, std::vector<cv::Vec2i> inkpts, std::vector<cv::Vec2i> papypts) {
+        double ink_avg = 0.0;  
+
+        for (auto i : inkpts)
+        {
+                ink_avg += image.at<float>(i[0], i[1]);
+        }
+
+        ink_avg /= inkpts.size();
+
+        double papy_avg = 0.0;
+
+        for (auto i : papypts)
+        {
+                papy_avg += image.at<float>(i[0], i[1]);
+        }
+
+        papy_avg /= papypts.size();
+
+        double contrast = abs((ink_avg - papy_avg)/(ink_avg + papy_avg));
+
+
+        //cout << return_wavelength(paths) << "       " << contrast<< endl;
+        //cout << return_wavelength(paths) << endl;
+        //cout << contrast << endl;
+        //cout << ink_avg << endl;
+        //cout << papy_avg << endl;
+
+        return contrast;
+
+}
+
+// returns contrast of a subregion using root mean square metric
+double rms_contrast(cv::Mat image,box region) {
+        cv::Mat subImg = image(cv::Range(region.ymin,region.ymax), cv::Range(region.xmin,region.xmax)).clone();
+
+        //normalize(subImg,subImg);
+        double min, max;
+
+        cv::minMaxLoc(subImg,&min,&max);
+
+        subImg = (subImg - min)/(max - min);
+
+        // compute average intensity
+        double avgIntensity = 0.0;
+        double imageSize = (region.ymax - region.ymin) * (region.xmax - region.xmin);
+
+        cv::Scalar matMean = cv::mean(subImg);
+        avgIntensity = matMean.val[0];
+
+        // compute contrast
+        double contrast = 0.0;
+        for (int y = region.ymin; y < region.ymax; y++)
+        {
+                for (int x = region.xmin; x < region.xmax; x++)
+                {
+                        contrast += pow((subImg.at<float>(y,x) - avgIntensity),2);
+                }
+        }
+
+        contrast = sqrt((1/imageSize)*contrast);
+
+        return contrast;
+}
+
 // given path to image (wavelength).tif, return wavelength
 string return_wavelength(fs::path imagePath) {
-	auto name = imagePath.stem().string();
+        auto name = imagePath.stem().string();
 
-	int pos3 = name.find("-");
-	if (pos3 !=string::npos)
-	{
-		name.replace(pos3,1,".");
-	}
-	
-	return name;
+        int pos3 = name.find("-");
+        if (pos3 !=string::npos)
+        {
+                name.replace(pos3,1,".");
+        }
+
+        return name;
 }
 
 // argv[1] == directory of wavelengths to be passed
 int main(int argc, char** argv) {
-	/*
-	if (argc != 2)
-	{
-		cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
-		return -1;
-	}
-	*/
-	std::vector<fs::path> imgpaths;
-	
-	get_all(argv[1], ".tif", imgpaths);	
+        /*
+        if (argc != 2)
+        {
+                cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+                return -1;
+        }
+        */
+        std::vector<fs::path> imgpaths;
 
-	std::vector<cv::Vec2i> inkpts, papypts;
-        
-	/* Random points of ink
-	inkpts.push_back(cv::Vec2i(1703,658));
+        get_all(argv[1], ".tif", imgpaths);
+
+        //std::vector<cv::Vec2i> inkpts, papypts;
+
+        /* Random points of ink
+        inkpts.push_back(cv::Vec2i(1703,658));
         inkpts.push_back(cv::Vec2i(1635,667));
         inkpts.push_back(cv::Vec2i(1706,584));
         inkpts.push_back(cv::Vec2i(1657,747));
         inkpts.push_back(cv::Vec2i(1545,539));
 
-	inkpts.push_back(cv::Vec2i(1046,1177));
-	inkpts.push_back(cv::Vec2i(1002,1183));
-	inkpts.push_back(cv::Vec2i(1536,502));
-	inkpts.push_back(cv::Vec2i(1498,497));
-	inkpts.push_back(cv::Vec2i(1680,671));
-	*/
-	
-	/* Regions of papyrus with randomly generated points	
-	box papyrus_box, papyrus_box1, papyrus_box2, papyrus_box3;
-	papyrus_box.xmin = 516;
-	papyrus_box.xmax = 528;
-	papyrus_box.ymax = 1561;
-	papyrus_box.ymin = 1402;
+        inkpts.push_back(cv::Vec2i(1046,1177));
+        inkpts.push_back(cv::Vec2i(1002,1183));
+        inkpts.push_back(cv::Vec2i(1536,502));
+        inkpts.push_back(cv::Vec2i(1498,497));
+        inkpts.push_back(cv::Vec2i(1680,671));
+        */
 
-	papyrus_box1.xmin = 723;
+        /* Regions of papyrus with randomly generated points    
+        box papyrus_box, papyrus_box1, papyrus_box2, papyrus_box3;
+        papyrus_box.xmin = 516;
+        papyrus_box.xmax = 528;
+        papyrus_box.ymax = 1561;
+        papyrus_box.ymin = 1402;
+	
+        papyrus_box1.xmin = 723;
         papyrus_box1.xmax = 737;
         papyrus_box1.ymax = 1768;
         papyrus_box1.ymin = 1624;
 
-	papyrus_box2.xmin = 559;
+        papyrus_box2.xmin = 559;
         papyrus_box2.xmax = 574;
         papyrus_box2.ymax = 1713;
         papyrus_box2.ymin = 1567;
 
-	papyrus_box3.xmin = 773;
+        papyrus_box3.xmin = 773;
         papyrus_box3.xmax = 781;
         papyrus_box3.ymax = 1715;
         papyrus_box3.ymin = 1638;
 
-	for (int i=0; i<500; i++)
-	{
-		papypts.push_back(cv::Vec2i(random(papyrus_box.ymin,papyrus_box.ymax),random(papyrus_box.xmin,papyrus_box.xmax)));
-	}
-	*/
+        for (int i=0; i<500; i++)
+        {
+                papypts.push_back(cv::Vec2i(random(papyrus_box.ymin,papyrus_box.ymax),random(papyrus_box.xmin,papyrus_box.xmax)));
+        }
+        */
 
-	/* Random points of papyrus
+        /* Random points of papyrus
         papypts.push_back(cv::Vec2i(1704,693));
         papypts.push_back(cv::Vec2i(1764,725));
         papypts.push_back(cv::Vec2i(1685,560));
@@ -126,106 +191,76 @@ int main(int argc, char** argv) {
         papypts.push_back(cv::Vec2i(1505,522));
 
         papypts.push_back(cv::Vec2i(1030,1059));
-	papypts.push_back(cv::Vec2i(1187,990));
+        papypts.push_back(cv::Vec2i(1187,990));
         papypts.push_back(cv::Vec2i(1528,459));
         papypts.push_back(cv::Vec2i(1044,1194));
         papypts.push_back(cv::Vec2i(1472,470));
-	*/
+        */
 
-	box inkPapy;
-	inkPapy.xmin = 473;
-	inkPapy.xmax = 532;
-	inkPapy.ymin = 1429;
-	inkPapy.ymax = 1559;
+        box inkPapy0;
+        inkPapy0.xmin = 473;
+        inkPapy0.xmax = 532;
+        inkPapy0.ymin = 1429;
+        inkPapy0.ymax = 1559;
 
-	cv::Mat image;
-	
-	for (auto paths : imgpaths)
-	{
-			
-		image = imread(paths.string(),-1);	
-		if (!image.data)
-		{
-			cout << "Could not open or find the image" << endl;
-			cout << paths.string() << endl;
-			return -1;
-		}
+        box inkPapy1;
+        inkPapy1.xmin = 730;
+        inkPapy1.xmax = 780;
+        inkPapy1.ymin = 1640;
+        inkPapy1.ymax = 1770;
 
-		/*
-		// Michelson Contrast Metric
-		double ink_avg = 0.0;	
+        box inkPapy2;
+        inkPapy2.xmin = 516;
+        inkPapy2.xmax = 605;
+        inkPapy2.ymin = 1649;
+        inkPapy2.ymax = 1699;
 
-		for (auto i : inkpts)
-		{
-			ink_avg += image.at<float>(i[0], i[1]);
-		}
+        box inkPapy3;
+        inkPapy3.xmin = 642;
+        inkPapy3.xmax = 718;
+        inkPapy3.ymin = 1672;
+        inkPapy3.ymax = 1721;
 
-		ink_avg /= inkpts.size();
+        cv::Mat image;
 
-		//cout << "Ink average: " << ink_avg << endl;
-		
-		double papy_avg = 0.0;
+        ofstream dataOutput;
+        dataOutput.open("results.csv");
 
-        	for (auto i : papypts)
-        	{
-        	        papy_avg += image.at<float>(i[0], i[1]);
-        	}
+        dataOutput << "Wavelength," << "Region0," << "Region1," << "Region2," << "Region3" << endl << endl;
+        cout << "Wavelength," << "Region0," << "Region1," << "Region2," << "Region3" << endl << endl;
 
-        	papy_avg /= papypts.size();
-		
-		double contrast = abs((ink_avg - papy_avg)/(ink_avg + papy_avg));
-		
-		
-		//cout << return_wavelength(paths) << "       " << contrast<< endl;
-		//cout << return_wavelength(paths) << endl;
-		//cout << contrast << endl;
-		cout << ink_avg << endl;
-		//cout << papy_avg << endl;
-		*/
-		
-		// RMS Contrast
-		
-		// Normalize region for all intensities
-		cv::Mat subImg = image(cv::Range(inkPapy.ymin,inkPapy.ymax), cv::Range(inkPapy.xmin,inkPapy.xmax)).clone();
-		
-		normalize(subImg,subImg);
-		
-		// compute average intensity
-		double avgIntensity = 0.0;
-		double imageSize = (inkPapy.ymax - inkPapy.ymin) * (inkPapy.xmax - inkPapy.xmin);
-		
-		cv::Scalar matMean = cv::mean(subImg);
-		avgIntensity = matMean.val[0];
-		
-		// compute contrast
-		double contrast = 0.0;
-		for (int y = inkPapy.ymin; y < inkPapy.ymax; y++)
+        for (auto paths : imgpaths)
+        {
+
+                image = imread(paths.string(),-1);
+                if (!image.data)
                 {
-                        for (int x = inkPapy.xmin; x < inkPapy.xmax; x++)
-                        {
-                        	contrast += pow((subImg.at<float>(y,x) - avgIntensity),2);
-			}
+                        cout << "Could not open or find the image" << endl;
+                        cout << paths.string() << endl;
+                        return -1;
                 }
 
-		contrast = sqrt((1/imageSize)*contrast);
-		
-		/* Check size of subImg	
-		cv::Size s = subImg.size();
-		int mat_size = s.height * s.width;
+                double contrast0 = rms_contrast(image,inkPapy0);
+                double contrast1 = rms_contrast(image,inkPapy1);
+                double contrast2 = rms_contrast(image,inkPapy2);
+                double contrast3 = rms_contrast(image,inkPapy3);
 
-		cout << imageSize << "    " << mat_size << endl;
-		*/
-		
-		//cout << return_wavelength(paths) << endl;
-		cout << contrast << endl;
-	
-	}
-	
-	/*	
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display
-	imshow("Display window", image); // Show image inside window
-	
-	waitKey(0);
-	*/
-	return 0;
+
+                //cout << return_wavelength(paths) << endl;
+                dataOutput << return_wavelength(paths) << "," << contrast0 << "," << contrast1 << "," << contrast2 << "," << contrast3 << endl;
+
+                cout << return_wavelength(paths) << "," << contrast0 << "," << contrast1 << "," << contrast2 << "," << contrast3 << endl;
+
+        }
+
+        dataOutput.close();
+
+        /*      
+        namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display
+        imshow("Display window", image); // Show image inside window
+        
+        waitKey(0);
+        */
+
+        return 0;
 }
