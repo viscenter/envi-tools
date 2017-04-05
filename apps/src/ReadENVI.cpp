e412e6ad8db0bc62a19c1d492a2a5d0d5461409e
aconway01 <aconway01@gmail.com>
2017-04-05 17:29:36 -0400

    added program_options for extract app

--- apps/src/ReadENVI.cpp
+++ apps/src/ReadENVI.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 
@@ -10,12 +11,56 @@
 
 namespace et = envitools;
 namespace fs = boost::filesystem;
+namespace po = boost::program_options;
 
-int main(int /*argc*/, char* argv[])
+int main(int argc, char** argv)
 {
+    fs::path hdrPath;
+
+    // clang-format off
+    po::options_description options("Options");
+    options.add_options()
+        ("help,h","Show this message")
+        ("hdr-path,p", po::value<std::string>()->required(),
+            "Path of ENVI file")
+        ("band,b", po::value<int>()->required(),
+            "Band to extract");
+    // clang-format on
+
+    // parsedOptions will hold the value of all parsed options as a Map
+    po::variables_map parsedOptions;
+    po::store(
+        po::command_line_parser(argc, argv).options(options).run(),
+        parsedOptions);
+
+    // show the help message
+    if (parsedOptions.count("help") || argc < 3) {
+        std::cout << options << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    // warn of missing options
+    try {
+        po::notify(parsedOptions);
+    } catch (po::error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Get the hdr path
+    hdrPath = parsedOptions["hdr-path"].as<std::string>();
+
+    if (!(boost::filesystem::exists(hdrPath))) {
+        std::cerr << "Error: File path does not exist." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Get band info
+    auto band = parsedOptions["band"].as<int>();
+
     // Get args
-    fs::path hdrPath = argv[1];
-    int band = std::stoi(argv[2]);
+    // fs::path hdrPath = argv[1];
+    // int band = std::stoi(argv[2]);
 
     // Load ENVI file and print header info
     envitools::ENVI envi(hdrPath);
